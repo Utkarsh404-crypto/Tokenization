66844987dacbc286f4c498b3be08581cbb409869
Utkarsh404-crypto <uksingh2872000@gmail.com>
2022-11-29 01:17:53 +0530

    first commit

---
+++ truffle/contracts/MyTokenSale.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Crowdsale.sol";
+import "./KycContract.sol";
+
+contract MyTokenSale is Crowdsale {
+    KycContract kyc;
+
+    constructor(
+        uint256 rate, // rate in TKNbits
+        address payable wallet,
+        ERC20 token,
+        KycContract _kyc
+    ) public Crowdsale(rate, wallet, token) {
+        kyc = _kyc;
+    }
+
+    function _preValidatePurchase(address beneficiary, uint256 weiAmount)
+        internal
+        override
+    {
+        super._preValidatePurchase(beneficiary, weiAmount);
+        require(
+            kyc.kycCompleted(msg.sender),
+            "KYC Not completed, purchase not allowed"
+        );
+    }
+}
